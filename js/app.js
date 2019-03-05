4e4288d2f4830e230152119b4b120dfe00973a66
Mohamed Osama <mohamedosama98x@gmail.com>
2019-03-05 02:09:03 +0200

    Added starting guides

---
+++ js/app.js
@@ -0,0 +1,21 @@
+// Defining books class
+let books = [];
+class book {
+    constructor(name, author, year) {
+        this.bookName = name;
+        this.bookAuthor = author;
+        this.bookYear = year;
+    }
+
+    addBook() {
+
+    }
+
+    removeBook() {
+
+    }
+
+    editBook() {
+
+    }
+}
